6403d7cac68493bddb56774c85ba330982af28b9
FManga <fmanga@localhost.localdomain>
2019-05-26 14:42:23 +0100

    Update Java template

--- templates/3-Java Game/Main.java
+++ templates/3-Java Game/Main.java
@@ -5,7 +5,7 @@ import femto.input.Button;
 import femto.palette.Psygnosia;
 import femto.font.TIC80;
 
-class Level1 extends State {
+class Main extends State {
 
     HiRes16Color screen; // the screenmode we want to draw with
 
@@ -14,6 +14,12 @@ class Level1 extends State {
     float angle; // floats are actually FixedPoint (23.8)
     
     int counter; // variables are automatically initialized to 0 or null
+
+    // start the game using Main as the initial state
+    // and TIC80 as the menu's font
+    public static void main(String[] args){
+        Game.run( TIC80.font(), new Main() );
+    }
     
     // Avoid allocation in a State's constructor.
     // Allocate on init instead.
@@ -29,40 +35,36 @@ class Level1 extends State {
         screen = null;
     }
     
-    // called by femto.Game for every frame
+    // update is called by femto.Game every frame
     void update(){
         
+        // Change to a new state when A is pressed
+        if( Button.A.justPressed() )
+            Game.changeState( new Main() );
+
+        // Fill the screen using Pattern.png
         for( int y=0; y<176; y += background.height() ){
             for( int x=0; x<220; x += background.width() ){
                 background.draw(screen, x, y);
             }
         }
-        
-        if( Button.A.justPressed() ){
-            Game.changeState( new Level1() );
-        }
-        
-        screen.setTextPosition( 100, 84 );
+
+        // Print some text
         counter++;
+        screen.setTextPosition( 100, 84 );
+        screen.textColor++;
         screen.print((counter>>5)&1 ? "ROUND" : "AND");
         
-        angle += 0.03f;
-        
+        // Move and draw the dog
         float previousX = dog.x;
+        angle += 0.03f;
         dog.x = 80 + Math.cos(angle) * 60.0f;
         dog.y = 44 + Math.sin(angle) * 50.0f;
-
         dog.setMirrored( previousX > dog.x );
+        dog.draw(screen); // Animation is updated automatically
         
-        dog.draw(screen);
-        
+        // Update the screen with everything that was drawn
         screen.flush();
     }
     
-}
-
-public class Main {
-    public static void main(String[] args){
-        Game.run( TIC80.font(), new Level1() );
-    }
 }
