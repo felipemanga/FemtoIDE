15fbbaf5f69040292aac8feaf73565a472a7ef6c
FManga <fmanga@localhost.localdomain>
2019-08-10 18:44:59 +0100

    Add play method to Procedural class

--- javacompiler/femto/sound/Procedural.java
+++ javacompiler/femto/sound/Procedural.java
@@ -1,7 +1,12 @@
 package femto.sound;
 
 public class Procedural {
-    uint t;
+    public uint t;
+    public char channel;
+
+    public void play(){ Mixer.setChannel(channel, this); }
+
     public void reset(){ t = 0; }
+
     public byte update(){ return 0; }
 }
