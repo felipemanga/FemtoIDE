8f0a835e91fb28cb66e094ce9a35d16deeef02e0
FManga <fmanga@localhost.localdomain>
2019-09-19 19:24:40 +0100

    Optimize drawHLine 1/2

--- javacompiler/pokitto/femto/mode/HiRes16Color/drawHLine.cpp
+++ javacompiler/pokitto/femto/mode/HiRes16Color/drawHLine.cpp
@@ -8,18 +8,23 @@ if( x < 0 ){
     x = 0;
 }
 
-std::uint32_t screenWidth = width();
+const std::uint32_t screenWidth = 220;
+const std::uint32_t screenHeight = 176;
 
 if( (x + w) >= (int32_t) screenWidth )
     w = screenWidth - x;
 
-if( std::uint32_t(x)>=screenWidth || std::uint32_t(y)>=std::uint32_t(height()) || w<1 )
+if( std::uint32_t(x)>=screenWidth || std::uint32_t(y)>=screenHeight || w<1 )
     return;
         
 color = (color<<4) | (color&0xF);
+color |= color<<8;
+color |= color<<16;
 
+auto *b = buffer->elements+y*(screenWidth>>1)+(x>>1);
 if( x&1 ){
-    setPixel(x, y, color);
+    // setPixel(x, y, color);
+    *b++ = (*b & 0xF0) | (color&0xF);
     w--;
     x++;
     if(!w)
@@ -27,7 +32,17 @@ if( x&1 ){
 }
 
 int rem = (w>>1);
-auto *b = buffer->elements+y*(screenWidth>>1)+(x>>1);
+
+while( rem && (uintptr_t(b)&3)){
+    *b++ = color;
+    rem--;
+}
+
+while( rem >= 4 ){
+    *((uint32_t*)b) = color;
+    b += 4;
+    rem -= 4;
+}
 
 while( rem ){
     *b++ = color;
@@ -35,4 +50,5 @@ while( rem ){
 }
 
 if( w&1 )
-    setPixel( x+w-1, y, color );
+    *b = (*b & 0xF) | (color&0xF0);
+    // setPixel( x+w-1, y, color );
