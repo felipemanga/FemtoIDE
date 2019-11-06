83c98a55788aaa8e549c2992c71987a0e782a33f
FManga <fmanga@localhost.localdomain>
2019-11-06 23:55:20 +0000

    Button.waitForPress

--- javacompiler/femto/input/Button.java
+++ javacompiler/femto/input/Button.java
@@ -63,6 +63,33 @@ public class Button {
             });
     }
 
+    public static Button waitForPress(){
+        Button btn = null;
+        while( btn == null ){
+            if(A.isPressed()) btn = A;
+            else if(B.isPressed()) btn = B;
+            else if(C.isPressed()) btn = C;
+            else if(Up.isPressed()) btn = Up;
+            else if(Down.isPressed()) btn = Down;
+            else if(Left.isPressed()) btn = Left;
+            else if(Right.isPressed()) btn = Right;
+
+            if( Game.instance != null ){
+                State state = Game.instance.getState();
+                if( state == Game.instance.menuState ){
+                    while(Game.instance.state == Game.instance.menuState){
+                        Game.instance.menuState.update();
+                    }
+                    return Button.Reset;
+                }
+            }
+        }
+
+        while(btn.isPressed());
+        
+        return btn;
+    }
+
     /// Returns `true` if the button has just transition from being released to being pressed.
     public boolean justPressed(){
         var newState = isPressed();
@@ -92,7 +119,11 @@ public class Button {
     public static final Button Flash = new Button(0, 1);
 
     /// The `Button` object representing the Pokitto's reset button.
-    public static final Button Reset = new Button(0, 0);
+    public static final Button Reset = new Button(0, 0){
+            public boolean isPressed(){
+                return !super.isPressed();
+            }
+        };
 
     /// The `Button` object representing the Pokitto's up button.
     public static final Button Up = new Button(1, 13);
