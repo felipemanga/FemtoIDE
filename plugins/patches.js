6ea9cdba9dce50d7818563178ccd4d3d1e5e40c0
FManga <fmanga@localhost.localdomain>
2020-02-12 00:05:21 +0000

    Automatically update projects to C++17

---
+++ plugins/patches.js
@@ -0,0 +1,21 @@
+APP.addPlugin("Patches", [], _=>{
+    APP.add(new class Patches {
+        onOpenProject(){
+            const project = DATA.project;
+            try{ cpp17(project); }catch(ex){}
+        }
+    });
+
+    function cpp17(project){
+        Object.keys(project.CPPFlags).forEach(key=>{
+            let flags = project.CPPFlags[key];
+            if(Array.isArray(flags)){
+                let i = flags.indexOf("-std=c++14");
+                if(i != -1){
+                    flags[i] = "-std=c++17";
+                    APP.dirtyProject();
+                }
+            }
+        });
+    }
+});
